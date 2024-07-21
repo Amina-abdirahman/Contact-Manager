90cfd0d0115b7a57a100d52f4fb0dfe5c908f9dc
Amina-abdirahman <aminofarah19@gmail.com>
2024-07-21 12:20:20 +0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import About from './pages/About';
+import ContactPage from './pages/ContactPage';
+import LoginPage from './pages/LoginPage';
+import ContactManager from './pages/ContactManager';
+ // Corrected spelling if necessary
+import { ContactProvider } from './context/contactContext';
+import AddContact from './components/AddContectForm';
+import EditContact from './components/EditContectForm';
+
+function App() {
+  return (
+    <Router>
+      <ContactProvider>
+       
+        <Routes>
+          <Route path='/' element={<LoginPage />} />
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/dashboard' element={<ContactManager />} />
+          <Route path='/add' element={<AddContact />} />
+          <Route path='/edit/:id' element={<EditContact />} />
+          <Route path='/contactus' element={<ContactPage />} />
+          {/* Add other routes here */}
+        </Routes>
+      </ContactProvider>
+    </Router>
+  );
+}
+
+export default App;
