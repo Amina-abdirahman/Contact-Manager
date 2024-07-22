af59caf845b65a2342d95c6833b943231b8e63be
Amina-abdirahman <aminofarah19@gmail.com>
2024-07-22 10:56:33 +0300

    Commited.

--- src/components/utils/utils.jsx
+++
@@ -1,7 +0,0 @@
-export const emailValidator = email => {
-    return /\S+@\S+\.\S+/.test(email);
-};
-
-export const phoneValidator = phone => {
-    return /^\d{10}$/.test(phone);  // Simple US phone number validation
-};
