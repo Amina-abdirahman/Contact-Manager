a1dad5f346699e78f9f48b9f2cbcd1aa284437cd
Amina-abdirahman <aminofarah19@gmail.com>
2024-07-28 19:19:30 +0300

    Committing all changes.

--- src/components/contactList.jsx
+++ src/components/contactList.jsx
@@ -1,4 +1,14 @@
 import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ContactContext from '../context/contactContext';
+const ContactList = ({ contacts }) => {
+    const { deleteContact } = useContext(ContactContext);
+    const navigate = useNavigate();
+  
+    const handleDelete = (id) => {
+      deleteContact(id);
+    };
 
+
+    
+}
